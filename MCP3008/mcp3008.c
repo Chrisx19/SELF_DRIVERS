8926191f4dbc7d62fe2bb3250b2eaae074cdcdb1
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2024-03-27 21:18:06 -0700

    Update mcp3008.c

--- MCP3008/mcp3008.c
+++ MCP3008/mcp3008.c
@@ -7,37 +7,23 @@ MCP3008_Error_t MCP3008_Init(MCP3008_t *mcp, SPI_HandleTypeDef *hspi1)
 		return MCP3008_ERR_FAILURE;
 	} else {
 		mcp->hspi1 = hspi1;
-		mcp->mcpEnable = true;
-		mcp->txBuff = 0;
 		memset(mcp->rxBuff, 0x00, RX_SIZE);
 	}
 	printf("MCP3008 Initialized...\r\n");
 	return retVal;
 }
 
-static MCP3008_Error_t MCP3008_Enable(MCP3008_t *mcp)
+static void MCP3008_Enable(void)
 {
-	if (mcp == NULL) {
-		return MCP3008_ERR_FAILURE;
-	} else {
-		mcp->mcpEnable = false;
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, mcp->mcpEnable);
-	}
-	return MCP3008_ERR_SUCCESS;
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, false);
 }
 
-static MCP3008_Error_t MCP3008_Disable(MCP3008_t *mcp)
+static void MCP3008_Disable(void)
 {
-	if (mcp == NULL) {
-		return MCP3008_ERR_FAILURE;
-	} else {
-		mcp->mcpEnable = true;
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, mcp->mcpEnable);
-	}
-	return MCP3008_ERR_SUCCESS;
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, true);
 }
 
-/* Tx Data but dont care about Rx Data */
+
 static MCP3008_Error_t MCP3008_TransmitRegister(MCP3008_t *mcp, const uint8_t data)
 {
 	if (mcp == NULL) {
@@ -47,24 +33,21 @@ static MCP3008_Error_t MCP3008_TransmitRegister(MCP3008_t *mcp, const uint8_t da
 	if (data > 0xFF) {
 		return MCP3008_ERR_FAILURE;
 	}
-
-	mcp->txBuff = data;
-	HAL_SPI_Transmit(mcp->hspi1, &mcp->txBuff, 1, HAL_MAX_DELAY);
+	HAL_SPI_Transmit(mcp->hspi1, (uint8_t *)&data, 1, HAL_MAX_DELAY);
 	return MCP3008_ERR_SUCCESS;
 }
 
-/* Want Rx data but dont care about tx */
 static MCP3008_Error_t MCP3008_ReceiveRegister(MCP3008_t *mcp)
 {
 	if (mcp == NULL) {
 		return MCP3008_ERR_FAILURE;
 	}
 
-	HAL_SPI_Receive(mcp->hspi1, &mcp->rxBuff[1], 1, HAL_MAX_DELAY);
+	HAL_SPI_Receive_IT(mcp->hspi1, &mcp->rxBuff[1], 1);
 	return MCP3008_ERR_SUCCESS;
 
 }
-/* If Rx and Tx is important */
+
 static MCP3008_Error_t MCP3008_TransmitReceiveRegister(MCP3008_t *mcp, uint8_t const data)
 {
 	if (mcp == NULL) {
@@ -74,29 +57,35 @@ static MCP3008_Error_t MCP3008_TransmitReceiveRegister(MCP3008_t *mcp, uint8_t c
 	if (data > 0xFF) {
 		return MCP3008_ERR_FAILURE;
 	}
-	mcp->txBuff = data;
-	HAL_SPI_TransmitReceive(mcp->hspi1, &mcp->txBuff, &mcp->rxBuff[0], 1, HAL_MAX_DELAY);
+	HAL_SPI_TransmitReceive(mcp->hspi1, (uint8_t *)&data, &mcp->rxBuff[0], 1, HAL_MAX_DELAY);
 	return MCP3008_ERR_SUCCESS;
 }
 
+
+
 int16_t MCP3008_ReadChannel_1(MCP3008_t *mcp)
 {
-	int16_t rawVal = 0;
+	const uint8_t START_BIT = 0x01;
 	const uint8_t CH1_CONFIG = (MCP3008_SINGLE_ENDED | MCP3008_CHANNEL_0);
 	MCP3008_Error_t retVal = MCP3008_ERR_SUCCESS;
-	retVal |= MCP3008_Enable(mcp);
-	retVal |= MCP3008_TransmitRegister(mcp, MCP3008_START_BIT);
+	MCP3008_Enable();
+	retVal |= MCP3008_TransmitRegister(mcp, START_BIT);
 	retVal |= MCP3008_TransmitReceiveRegister(mcp, CH1_CONFIG);
 	retVal |= MCP3008_ReceiveRegister(mcp);
-	retVal |= MCP3008_Disable(mcp);
 
 	if (retVal != MCP3008_ERR_SUCCESS) {
 		return 0;
 	}
 
-	uint16_t adcMSB = (mcp->rxBuff[0] & 0x03) << 8;
+	uint8_t adcMSB = mcp->rxBuff[0] & 0x03;
 	uint8_t adcLSB = mcp->rxBuff[1];
 
-	rawVal = adcMSB | adcLSB;
-    return rawVal;
+	mcp->adcData = (adcMSB<<8) | adcLSB;
+    return mcp->adcData;
+}
+
+// This is called when SPI receive is done
+void HAL_SPI_RxCpltCallback (SPI_HandleTypeDef *hspi)
+{
+	MCP3008_Disable();
 }
